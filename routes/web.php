aca889429594660a0625d4421ac9238289901811
imperialbeast258-ops <imperialbeast258@gmail.com>
2025-11-18 16:58:57 +0800

    login,logout,register

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 }); #default
 
+Route::get('/register',[AuthController::class, 'showRegister'])->name('register.form');
+Route::post('/register',[AuthController::class, 'performRegister'])->name('register');
+
+Route::get('/login',[AuthController::class, 'showLogin'])->name('login.form');
+Route::post('/login',[AuthController::class, 'performLogin'])->name('login');
+
+Route::post('/logout', function(){
+    
+});
 
 Route::view('home', 'home'); #this is my first project
